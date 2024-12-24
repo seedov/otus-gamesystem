3bbf5e9380a19ad2c390e36bfc0a4cc62cbdd3f0
antonsedov <antonsedov>
2024-12-24 15:30:53 +0300

    скрипт и префаб турели

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,17 +1,7 @@
-using System;
-using System.Runtime.CompilerServices;
-using NUnit.Framework.Interfaces;
 using UnityEngine;
-using Zenject;
 
-public interface IBulletHit
-{
-    void Hit();
-}
 public class Bullet : MonoBehaviour
 {
-
-
     public void Update()
     {
         transform.position += transform.forward * 10 ;
